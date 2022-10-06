0b1c163690e8cb250a1f6c5d40dfec1be53b096f
martokk <25113632+martokk@users.noreply.github.com>
2022-10-06 02:40:57 -0500

    Improving dotfiles support

--- quick-capture/main.py
+++ quick-capture/main.py
@@ -3,13 +3,17 @@ from typing import Union, Dict
 import yaml
 import PySimpleGUI as sg
 
+CONFIG_PATH_DEFAULT = Path('./quick-capture/.vapps/quick-capture.yaml')
+CONFIG_PATH_DOTFILE = Path(f'{str(Path.home())}/.vapps/quick-capture.yaml')
+CONFIG_PATH = CONFIG_PATH_DOTFILE if CONFIG_PATH_DOTFILE.exists() else CONFIG_PATH_DEFAULT
+CONFIG_WARNING = not CONFIG_PATH_DOTFILE.exists()
+CONFIG = yaml.safe_load(open(CONFIG_PATH))
 
-config = yaml.safe_load(open(Path(f'{str(Path.home())}/.vapps/quick_capture.yaml')))
-
-CAPTURE_FILE_PATH = config.get('capture_file')
-THEME = config.get('theme', 'DarkGrey15')
-FONT = config.get('font', 'hack')
-FONT_SIZE = config.get('font_size', 14)
+CAPTURE_FILE_PATH = Path(CONFIG.get('capture_file'))
+CAPTURE_FILE_ERROR = not CAPTURE_FILE_PATH.exists()
+THEME = CONFIG.get('theme', 'DarkGrey15')
+FONT = CONFIG.get('font', 'hack')
+FONT_SIZE = CONFIG.get('font_size', 14)
 
 
 class QuickCapture:
@@ -21,6 +25,17 @@ class QuickCapture:
         layout = [
             [sg.Multiline(key='-IN-', enable_events=True, size=(60, 3), font=(FONT, FONT_SIZE))],
         ]
+
+        if CONFIG_WARNING:
+            warning = [sg.Text(text="WARNING! You are not using '~/.vapps/quick-capture.yaml dotfile'. See martokk/dotfiles to install.",
+                               text_color="orange")]
+            layout.append(warning)
+
+        if CAPTURE_FILE_ERROR:
+            error = [sg.Text(text=f"ERROR! Capture file '{CAPTURE_FILE_PATH}' does not exist. Text will not be saved. Update config dotfile.",
+                               text_color="red")]
+            layout.append(error)
+
         window = sg.Window('QuickCapture', layout, margins=(10, 20), no_titlebar=False, finalize=True)
         window.bind("<Return>", "Keypress_Return")
         window.bind("<Escape>", "Keypress_Escape")
