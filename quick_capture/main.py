6cc08e4802e8e9f9896f8a54a8958a7452d911fd
martokk <25113632+martokk@users.noreply.github.com>
2022-10-06 09:50:16 -0500

    Reformatted by VSCode / Black

--- quick_capture/main.py
+++ quick_capture/main.py
@@ -1,61 +1,82 @@
 from pathlib import Path
-from typing import Union, Dict
-import yaml
+from typing import Any, Dict, Union
+
 import PySimpleGUI as sg
+import yaml
 
-CONFIG_PATH_DEFAULT = Path('./quick_capture/.vapps/quick_capture.yaml')
-CONFIG_PATH_DOTFILE = Path(f'{str(Path.home())}/.vapps/quick_capture.yaml')
-CONFIG_PATH = CONFIG_PATH_DOTFILE if CONFIG_PATH_DOTFILE.exists() else CONFIG_PATH_DEFAULT
+CONFIG_PATH_DEFAULT = Path("./quick_capture/.vapps/quick_capture.yaml")
+CONFIG_PATH_DOTFILE = Path(f"{str(Path.home())}/.vapps/quick_capture.yaml")
+CONFIG_PATH = (
+    CONFIG_PATH_DOTFILE if CONFIG_PATH_DOTFILE.exists() else CONFIG_PATH_DEFAULT
+)
 CONFIG_WARNING = not CONFIG_PATH_DOTFILE.exists()
-CONFIG = yaml.safe_load(open(CONFIG_PATH))
+CONFIG = yaml.safe_load(open(CONFIG_PATH, encoding="utf-8"))
 
-CAPTURE_FILE_PATH = Path(CONFIG.get('capture_file'))
+CAPTURE_FILE_PATH = Path(CONFIG.get("capture_file"))
 CAPTURE_FILE_ERROR = not CAPTURE_FILE_PATH.exists()
-THEME = CONFIG.get('theme', 'DarkGrey15')
-FONT = CONFIG.get('font', 'hack')
-FONT_SIZE = CONFIG.get('font_size', 14)
+THEME = CONFIG.get("theme", "DarkGrey15")
+FONT = CONFIG.get("font", "hack")
+FONT_SIZE = CONFIG.get("font_size", 14)
 
 
 class QuickCapture:
-    def __init__(self):
+    def __init__(self) -> None:
         self.main()
 
     def main(self) -> None:
         sg.theme(THEME)
-        layout = [
-            [sg.Multiline(key='-IN-', enable_events=True, size=(60, 3), font=(FONT, FONT_SIZE))],
+        layout: list[Any] = [
+            [
+                sg.Multiline(
+                    key="-IN-", enable_events=True, size=(60, 3), font=(FONT, FONT_SIZE)
+                )
+            ],
         ]
 
         if CONFIG_WARNING:
-            warning = [sg.Text(text="WARNING! You are not using '~/.vapps/quick_capture.yaml dotfile'. See martokk/dotfiles to install.",
-                               text_color="orange")]
+            warning = [
+                sg.Text(
+                    text="WARNING! You are not using '~/.vapps/quick_capture.yaml dotfile'."
+                    " See martokk/dotfiles to install.",
+                    text_color="orange",
+                )
+            ]
             layout.append(warning)
 
         if CAPTURE_FILE_ERROR:
-            error = [sg.Text(text=f"ERROR! Capture file '{CAPTURE_FILE_PATH}' does not exist. Text will not be saved. Update config dotfile.",
-                               text_color="red")]
+            error = [
+                sg.Text(
+                    text=f"ERROR! Capture file '{CAPTURE_FILE_PATH}' does not exist. "
+                    "Text will not be saved. Update config dotfile.",
+                    text_color="red",
+                )
+            ]
             layout.append(error)
 
-        window = sg.Window('QuickCapture', layout, margins=(10, 20), no_titlebar=False, finalize=True)
+        window = sg.Window(
+            "QuickCapture", layout, margins=(10, 20), no_titlebar=False, finalize=True
+        )
         window.bind("<Return>", "Keypress_Return")
         window.bind("<Escape>", "Keypress_Escape")
 
         # Event Loop
         while True:
             event, values = window.read()
-            if event in [sg.WIN_CLOSED, 'Keypress_Escape']:
+            if event in [sg.WIN_CLOSED, "Keypress_Escape"]:
                 break
-            if event == 'Keypress_Return':
+            if event == "Keypress_Return":
                 self.save_value_to_file(value=values, file_path=CAPTURE_FILE_PATH)
                 break
         window.close()
 
     @staticmethod
-    def save_value_to_file(value: Dict[str, str], file_path: Union[str, Path], append=True) -> None:
-        with open(file_path, "a" if append else "w") as file:
+    def save_value_to_file(
+        value: Dict[str, str], file_path: Union[str, Path], append=True
+    ) -> None:
+        with open(file_path, "a" if append else "w", encoding="utf-8") as file:
             # print(value['-IN-'])
             file.write(f"- {value['-IN-'].removeprefix('-').strip()}\n")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     QuickCapture()
